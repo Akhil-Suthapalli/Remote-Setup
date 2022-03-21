1f6162412e28ae89ea8251f194d7a8c120d89e6a
Akhil Suthapalli <Suthapall.Akhil@tvsmotor.com>
2022-03-21 08:04:20 +0000

    Seperate camera added, functionaliy imporved

--- Camera_Test.py
+++ Camera_Test.py
@@ -1,60 +1,9 @@
-
-from picamera.array import PiRGBArray
 from picamera import PiCamera
+
 import time
-import cv2
-import tkinter as tk
-import numpy as np
-from PIL import Image
-from PIL import ImageTk
-from threading import Thread
- 
- 
-canvas = tk.Tk()
-canvas.geometry("800x600")
-b = tk.Label(canvas)
-b.pack() 
- 
-def TAKEFOTO():
-    camera = PiCamera()
-    camera.resolution = (320, 240)
-    camera.framerate = 32
-    rawCapture = PiRGBArray(camera, size=(320, 240))
-  
-# allow the camera to warmup
-    time.sleep(0.1)
- 
-# capture frames from the camera
-    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        image = frame.array
- 
-        (width,height,other) = image.shape
-     
- 
-     
-        value = image.item(100,100,0)
-    for x in range (0,width):
-        if(image.item(x,100,0) < 10):
-            cv2.rectangle(image,(x,height-100),(0,20),(255,255,255),2)
-            break;
-         
-         
-    print(value)
-    key = cv2.waitKey(1) & 0xFF
-    # clear the stream in preparation for the next frame
-    image = Image.fromarray(image)
-    image = ImageTk.PhotoImage(image)
-    b.image = image
-    rawCapture.truncate(0)
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
- 
- 
- 
-#canvas.pack()
- 
-canvas.mainloop()
-     
-t = Thread (target=TAKEFOTO)
-t.start()
+
+cam = PiCamera()
+cam.resolution = (640, 480)
+cam.start_preview()
+time.sleep(10)
+cam.stop_preview()
--- GUI.py
+++ GUI.py
@@ -1,6 +1,13 @@
 import tkinter as tk
-from tkinter import Button, Label,PhotoImage
+from tkinter import Button, Label,PhotoImage, Frame
+from picamera import PiCamera
 import RPi.GPIO as gpio
+
+import threading, time
+from picamera.array import PiRGBArray
+from PIL import Image, ImageTk
+import io
+
 gpio.setmode(gpio.BCM)
 gpio.setwarnings(False)
 
@@ -13,6 +20,16 @@ ign_switch_on = True
 root = tk.Tk()
 on = PhotoImage(file = "on.png")
 off = PhotoImage(file = "off.png")
+white = PhotoImage(file = "on.png",)
+
+#Camera
+camera = PiCamera()
+camera_on = True
+seperate_camera_on = True
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+time.sleep(0.1)
 
 def ign_switch_pressed():
     
@@ -27,7 +44,63 @@ def ign_switch_pressed():
         ign_switch_on = True
         gpio.output(ign_pin, gpio.HIGH) 
     
+def thread_for_image():
+    global camera_on, image_frame
+    j = "a0"
+    stream = io.BytesIO()
     
+    while not camera_on:
+        camera.capture(stream, format='jpeg')
+        stream.seek(0)
+        image = Image.open(stream)
+        image2 = ImageTk.PhotoImage(image)
+        image_frame.config(image = image2)
+        time.sleep(0.3)
+        stream.seek(0)
+        stream.truncate(0)
+    
+    """
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = Image.fromarray(frame.array)
+        image2 = ImageTk.PhotoImage(image)
+    # show the frame
+        image_frame.config(image = image2)
+    # clear the stream in preparation for the next frame
+        rawCapture.truncate(0)
+    # if the `q` key was pressed, break from the loop
+        if not camera_on:
+            break
+        """
+def seperate_camera_switch():
+    
+    global seperate_camera_on, seperate_camera_button
+    
+    if seperate_camera_on:
+        camera.start_preview(fullscreen=False, window = (680, 280, 640, 480))
+        seperate_camera_button.config(text = " Stop Seperate Camera")
+        seperate_camera_on = False
+    else:
+        camera.stop_preview()
+        seperate_camera_on = True
+        seperate_camera_button.config(text = "Start Seperate Camera")
+    
+    
+
+def camera_switch():
+    global camera_button, camera_on
+    
+    if camera_on:
+        camera_on = False
+        camera_button.config(text = "Stop Camera")
+        #camera.start_preview()
+        threading.Thread(target = thread_for_image).start()
+        #time.sleep(10)
+        #camera.stop_preview
+        
+    else:
+        camera_on = True
+        #camera.stop_preview()
+        camera_button.config(text = "Start Camera")
 
 if __name__ =="__main__":
     
@@ -40,9 +113,15 @@ if __name__ =="__main__":
     ign_switch = Button(root, image = off, command = ign_switch_pressed)
     ign_switch.place(x=200, y = 20)
     
+    camera_button  =Button (root, text = "Start Camera", font=("Arial", 20),command = camera_switch)
+    camera_button.place(x= 10, y= 150)
+    seperate_camera_button  =Button (root, text = "Start Camera Seperately", font=("Arial", 20),command = seperate_camera_switch)
+    seperate_camera_button.place(x= 200, y= 150)
     
-    
-    
+    frame = Frame(root, width=640, height=480)
+    frame.place(x= 10, y = 220)
+    image_frame = Label(frame, image = white)
+    image_frame.pack()
     
     root.mainloop()
     
