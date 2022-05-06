daee59eb45a6de9a5748eaf9fcc09bf0fbd2aeb5
Suthapalli.Akhil <Suthapalli.Akhil@tvsmotor.com>
2022-05-06 15:00:35 +0530

    Adding Sniffer API for NRF board

---
+++ BLE Sniffer.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  3 11:40:02 2022
+
+@Author: Akhil Suthapalli - R&D
+@Contact: Suthapalli.Akhil@tvsmotor.com
+@Mobile: +91 9494475575
+"""
+
+import SnifferAPI
